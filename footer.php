85d4fd6d152bf750bfc1157730646ca9496cdec1
Claude Ayitey <clauditey@gmail.com>
2018-01-25 21:57:43 +0000

    styled footer, removed div soup in footer.

--- footer.php
+++ footer.php
@@ -12,20 +12,9 @@
 ?>
 
 	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'claude-ayitey' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'claude-ayitey' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'claude-ayitey' ), 'claude-ayitey', '<a href="http://ayitey.me">Claude Ayitey</a>' );
-			?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer>
+		<span>Copyleft, <?php echo date( 'Y' ); ?> <span class="sep"> | </span> Claude Ayitey</span>
+	</footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
