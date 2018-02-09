74019f6ab6000d5b435b1408f7545133e5356029
Claude Ayitey <clauditey@gmail.com>
2018-02-09 01:27:13 +0000

    added a function to strip img tags off paragraph tags.

--- functions.php
+++ functions.php
@@ -157,3 +157,9 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+// Remove P Tags Around Images
+function filter_ptags_on_images($content){
+	return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
+
+add_filter('the_content', 'filter_ptags_on_images');
